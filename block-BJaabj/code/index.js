350b5ad333fab4299f6d89770cf0657f2f527c6a
KASHISH <kashu80525@gmail.com>
2023-04-11 22:51:52 +0530

    learning functions in javascript and wirting codes for that

--- block-BJaabj/code/index.js
+++ block-BJaabj/code/index.js
@@ -1,7 +1,9 @@
 /*
 1. Create a function named `sayHello` that alerts a message `Hello World!`.
 */
-
+function sayHello(){
+  alert(`Hello World!`);
+}
 /*
 2. Create a function named `fullName` with the following steps:
 
@@ -10,6 +12,13 @@
   - Concat the value of fistName and lastName and store it in another variable named `fullName`.
   - Alert `fullName`
 */
+function fullName(){
+  let firstName = prompt(`Enter the first name`);
+  let lastName = prompt(`Enter the last name`);
+
+  let fullName = firstName + "" + lastName;
+  alert(fullName);
+}
 
 /*
 3. Create a function named `addTwoNumbers` with the following steps:
@@ -19,7 +28,12 @@
   - Add the value of firstNum and secondNum and store it in another variable named `sum`.
   - Alert `sum`
 */
-
+function addTwoNumbers(){
+  let firstNum = +prompt(`Enter the first number`);
+  let secondNum = +prompt(`Enter the second number`);
+  let sum = firstNum + secondNum;
+  alert(sum);
+}
 /*
 4. Create a function named `getTable` with the following steps:
 
@@ -38,14 +52,24 @@
   9 * 9 = 81
   9 * 10 = 90
 */
-
+function getTable() {
+  
+}
 /*
 5. Create a function named `isLeapYear` with the following steps:
 
   - Prompts a message saying `Enter a year` and store the value into variable named`year`
   - Alert `[year] is a leap year` otherwise `[year] is not a leap year`
 */
+function isLeapYear() {
+  let year = +prompt(`Enter a year`);
 
+  if (year % 4 == 0){
+    alert(`Year is the leap year`);
+  }else {
+    alert(`Year is not a leap year`);
+  }
+}
 /*
 6. Create a function named `getFactorial` with the following steps:
 
@@ -53,3 +77,9 @@
   - Alert `The factorial of [number] is [factorial]`
 
 */
+function getFactorial() {
+  let number = +prompt(`Enter a number`);
+
+  alert(`The factorial of ${number} is ${factorial}`);
+
+}
