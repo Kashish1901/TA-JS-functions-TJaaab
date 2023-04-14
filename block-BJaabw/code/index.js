3ff2b406a9ce99b90ecbf67e6106d69e87e7a11e
KASHISH <kashu80525@gmail.com>
2023-04-14 16:01:43 +0530

    code for performing different operations

--- block-BJaabw/code/index.js
+++ block-BJaabw/code/index.js
@@ -11,11 +11,14 @@ minToSec(2) ➞ 120
 */
 
 // - Write a Function Decleration for above problem
-function minToSec() {
-  // Your code
+function minToSec(min) {
+ let seconds = min * 60 ;
+ return `${min} min is ${seconds}sec`;
 }
 // - Execute the function with required parameter
-
+minToSec(50);
+minToSec(13);
+minToSec(2) ;
 /* 2. 
 Create a function named isInRange which validates whether a number n is exclusively within the bounds of lower and upper.
 Return true and false based on that.
@@ -26,8 +29,12 @@ isInRange(1, 10, 19); // false
 */
 
 // - Write a Function Decleration for above problem
-function isInRange() {
-  // Your code
+function isInRange(l , u , n) {
+  if (n > l && n < u){
+    return true;
+  } else  {
+    return false;
+  }
 }
 // - Execute the function with required parameter
 
@@ -49,8 +56,17 @@ Obese: BMI is 30 or more
 
 */
 
-function calculateBMI() {
-  // Your code
+function calculateBMI(weight , height) {
+ let bmi = weight / (height * height);
+ if (bmi < 18.5){
+  return `underWeight`;
+ }else if ( bmi > 18.5 && bmi < 24.9) {
+  return `Normalweight`;
+ }else if ( bmi > 25 && bmi < 29.9) {
+  return `Overweight`;
+ }else if ( bmi > 30) {
+  return `Obese`;
+ }
 }
 
 /* 3. appropiateDrinks
@@ -64,8 +80,16 @@ Create a function that take the age are return the appropiate drink based on the
 
 */
 
-function appropiateDrinks() {
-  // Your code
+function appropiateDrinks(age) {
+  if (age < 14){
+    return `drink fruit juice`;
+  }else if (age > 14 && age < 18){
+    return `drink soda`;
+  }else if (age > 18 && age < 21){
+    return `drink fruit-flavored beer`;
+  }else if (age >= 21){
+    return `drink throat-piercing vodka`;
+  }
 }
 
 /* 4. Add two numers or string
@@ -80,7 +104,6 @@ Twist is when user passes anything other than number, or string value you should
 */
 
 function sum() {
-  // Your code
 }
 
 // Function Test
